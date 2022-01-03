97b3cc4d3fbfa5cf80abe98430ffb4396c2bee4f
Alessandro Degano <a.degano@gmail.com>
2022-01-03 16:43:11 +0100

    Add second part of day1 2021

--- 2021/day01.go
+++ 2021/day01.go
@@ -18,16 +18,34 @@ func main() {
 	}
 	contents := string(bytes)
 	split := strings.Split(contents, "\n")
+	depths := make([]int, len(split))
+	for i, s := range split {
+		depths[i], _ = strconv.Atoi(s)
+	}
 
 	// part 1
 	last := -1
 	increments := 0
-	for _, s := range split {
-		i, _ := strconv.Atoi(s)
-		if last != -1 && i > last {
+	for _, s := range depths {
+		if last != -1 && s > last {
 			increments++
 		}
-		last = i
+		last = s
 	}
-	fmt.Println(increments)
+	fmt.Printf("The number of increments is %d\n", increments)
+
+	// part 2
+	last = -1
+	increments = 0
+	for i := range depths {
+		if i >= len(depths)-2 {
+			break
+		}
+		window := depths[i] + depths[i+1] + depths[i+2]
+		if last != -1 && window > last {
+			increments++
+		}
+		last = window
+	}
+	fmt.Printf("The number of window increments is %d\n", increments)
 }
