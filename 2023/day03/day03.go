50428d82b4692b326d5d694a5cb895869d43aa30
Alessandro Degano <a.degano@gmail.com>
2023-12-03 14:44:19 +0100

    Add part2 of day03

--- 2023/day03/day03.go
+++ 2023/day03/day03.go
@@ -16,7 +16,7 @@ type Bounds struct {
 	minX, maxX, minY, maxY int
 }
 
-func isPart(lines []string, posX int, posY int, b Bounds) bool {
+func isPart(lines []string, posX int, posY int, b Bounds) (bool, int) {
 	for i:=posX-1; i<=posX+1; i++ {
 		if i < b.minX || i > b.maxX {
 			continue
@@ -26,11 +26,15 @@ func isPart(lines []string, posX int, posY int, b Bounds) bool {
 				continue
 			}
 			if ! unicode.IsDigit(rune(lines[i][j])) && lines[i][j] != '.' {
-				return true
+				if lines[i][j] == '*' {
+					return true, i*1000+j // This part `could` be part of a gear
+				} else {
+					return true, -1 // This part is not part of a gear
+				}
 			}
 		}
 	}
-	return false
+	return false, -1
 }
 
 func main() {
@@ -44,8 +48,8 @@ func main() {
 	bounds := Bounds{0, len(lines)-2, 0, len(lines[0])-1} // The x boundaries needs to also remove the last empty line
 	buffer := []string{}
 	save := false
-	partNumber := 0
-	partsSum := 0
+	var partNumber, gear, partsSum int
+	gearCandidates := make(map[int][]int)
 	// part 1
 	for x, line := range lines {
 		if len(line) < 1 {
@@ -56,10 +60,7 @@ func main() {
 				buffer = append(buffer, string(r))
 				// Check if there is a special character above, below, left, right or diagonal from this digit
 				if ! save {
-					save = isPart(lines, x, y, bounds)
-				}
-				if save {
-					fmt.Printf("Digit %c at (%d, %d) is a part number\n", r, x, y)
+					save, gear = isPart(lines, x, y, bounds)
 				}
 			} else {
 				if save {
@@ -67,10 +68,22 @@ func main() {
 					partsSum += partNumber
 					save = false
 					fmt.Printf("Part number %d saved.\n", partNumber)
+					if gear >= 0 {
+						gearCandidates[gear] = append(gearCandidates[gear],partNumber)
+						fmt.Printf("Gear candidate %d saved.\n", gear)
+					}
 				}
 				buffer = []string{}
 			}
 		}
 	}
 	fmt.Printf("The sum of the parts is %d\n", partsSum)
+	fmt.Printf("The gear candidates are %v\n", gearCandidates)
+	gearRatio := 0
+	for _, parts := range gearCandidates {
+		if len(parts) == 2 {
+			gearRatio += parts[0] * parts[1]
+		}
+	}
+	fmt.Printf("The gear ratio is %d\n", gearRatio)
 }
