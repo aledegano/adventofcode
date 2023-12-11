a23ad3f438a02e1859e275bb3b52e01bddd756e5
Alessandro Degano <a.degano@gmail.com>
2023-12-11 11:55:40 +0100

    Add part 2 solved.

--- 2023/day11/day11.go
+++ 2023/day11/day11.go
@@ -10,6 +10,7 @@ import (
 
 var inputFile = flag.String("inputFile", "test_input.txt", "Relative file path to use as input.")
 var debug = flag.Bool("debug", false, "Print debug info")
+var part = flag.Int("part", 1, "Which part of the puzzle to solve")
 
 type Coord struct {
 	x int
@@ -50,22 +51,30 @@ func main() {
 		deltaY := 0
 		for row, empty := range emptyRows {
 			if empty && coord.x > row {
-				deltaX++
+				if *part == 1 {
+					deltaX++
+				} else {
+					deltaX+=999999 // we need to replace 1 row with 1M rows, so we need to add 999999
+				}
 			}
 		}
 		for col, empty := range emptyCols {
 			if empty && coord.y > col {
-				deltaY++
+				if *part == 1 {
+					deltaY++
+				} else {
+					deltaY+=999999
+				}
 			}
 		}
 		initialPhoto[i] = Coord{coord.x + deltaX, coord.y + deltaY}
 	}
-	// evaluate distances the brute way
-	part1Result := 0
+	// evaluate distances between all pair of galaxies
+	result := 0
 	for i, coord := range initialPhoto {
 		for j:=i+1; j<len(initialPhoto); j++ {
-			part1Result += int(math.Abs(float64((initialPhoto[j].x - coord.x)))) + int(math.Abs(float64(coord.y - initialPhoto[j].y)))
+			result += int(math.Abs(float64((initialPhoto[j].x - coord.x)))) + int(math.Abs(float64(coord.y - initialPhoto[j].y)))
 		}
 	}
-	fmt.Printf("[Part 1] Sum of the shortest distance: %d\n", part1Result)
+	fmt.Printf("[Part %d] Sum of the shortest distance: %d\n", *part, result)
 }
