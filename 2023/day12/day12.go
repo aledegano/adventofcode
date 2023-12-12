0ef897961d4fd44bf30c855b473a520ed5fbe674
Alessandro Degano <a.degano@gmail.com>
2023-12-12 09:24:33 +0100

    Add day 12 parsing.

---
+++ 2023/day12/day12.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	// "unicode"
+)
+
+var inputFile = flag.String("inputFile", "test_input.txt", "Relative file path to use as input.")
+var debug = flag.Bool("debug", false, "Print debug info")
+
+func main() {
+	flag.Parse()
+	bytes, err := os.ReadFile(*inputFile)
+	if err != nil {
+		return
+	}
+	contents := string(bytes)
+	lines := strings.Split(contents, "\n")
+	springs := map[int]string{}
+	errors := map[int][]int{}
+	for i, line := range lines {
+		if line == "" {
+			continue
+		}
+		springs[i] = strings.Split(line, " ")[0]
+		for _,e := range strings.Split(strings.Split(line, " ")[1], ",") {
+			err, _ := strconv.Atoi(e)
+			errors[i] = append(errors[i], err)
+		}
+	}
+	fmt.Printf("The springs are %v and errors: %v\n", springs, errors)
+}
