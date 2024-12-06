0ae4037cef4518dff0cfbc82329721ea80d56401
Alessandro Degano <a.degano@gmail.com>
2024-12-06 15:22:46 +0100

    add part 2 that works on example but not on input.

--- 2024/day06/day06.go
+++ 2024/day06/day06.go
@@ -10,6 +10,35 @@ import (
 var inputFile = flag.String("inputFile", "test_input.txt", "Relative file path to use as input.")
 var debug = flag.Bool("debug", false, "Print debug info")
 
+type Position struct {
+	x, y int
+}
+
+var move = map[string]Position{
+	"<": Position{-1, 0},
+	">": Position{1, 0},
+	"^": Position{0, -1},
+	"v": Position{0, 1},
+}
+
+var rotateMove = map[string]string{
+	"<": "^",
+	"^": ">",
+	">": "v",
+	"v": "<",
+}
+
+func moveGuard(currentPosition Position, direction string) Position {
+	return Position{
+		currentPosition.x + move[direction].x,
+		currentPosition.y + move[direction].y,
+	}
+}
+
+func outsideMap(position Position, labMap [][]string) bool {
+	return position.x < 0 || position.x >= len(labMap[0]) || position.y < 0 || position.y >= len(labMap)
+}
+
 func main() {
 	flag.Parse()
 	bytes, err := os.ReadFile(*inputFile)
@@ -20,23 +49,9 @@ func main() {
 	contents := string(bytes)
 	lines := strings.Split(contents, "\n")
 	labMap := make([][]string, len(lines)-1)
-	type Position struct {
-		x, y int
-	}
 	guardPosition := Position{0, 0}
-	move := map[string]Position{
-		"<": Position{-1, 0},
-		">": Position{1, 0},
-		"^": Position{0, -1},
-		"v": Position{0, 1},
-	}
-	rotateMove := map[string]string{
-		"<": "^",
-		"^": ">",
-		">": "v",
-		"v": "<",
-	}
-	uniqueVisitedPositions := make(map[Position]bool)
+
+	uniqueVisitedPositions := make(map[Position]string)
 	for y, line := range lines {
 		if len(line) == 0 {
 			continue
@@ -51,27 +66,40 @@ func main() {
 		}
 	}
 	newGuardPosition := guardPosition
+	loops := 0
 	for {
-		newGuardPosition = Position{
-			guardPosition.x + move[labMap[guardPosition.y][guardPosition.x]].x,
-			guardPosition.y + move[labMap[guardPosition.y][guardPosition.x]].y,
-		}
-		newMove := labMap[guardPosition.y][guardPosition.x]
+		direction := labMap[guardPosition.y][guardPosition.x]
+		newGuardPosition = moveGuard(guardPosition, direction)
 		// if the guard exited the map break the loop
-		if newGuardPosition.x < 0 || newGuardPosition.x >= len(labMap[0]) || newGuardPosition.y < 0 || newGuardPosition.y >= len(labMap) {
+		if outsideMap(newGuardPosition, labMap) {
 			break
 		}
 		if labMap[newGuardPosition.y][newGuardPosition.x] == "#" { // Rotate 90 degrees to the right
-			newMove = rotateMove[labMap[guardPosition.y][guardPosition.x]]
-			newGuardPosition = Position{
-				guardPosition.x + move[newMove].x,
-				guardPosition.y + move[newMove].y,
-			}
+			direction = rotateMove[labMap[guardPosition.y][guardPosition.x]]
+			newGuardPosition = moveGuard(guardPosition, direction)
 		}
 		labMap[guardPosition.y][guardPosition.x] = "."
-		labMap[newGuardPosition.y][newGuardPosition.x] = newMove
+		labMap[newGuardPosition.y][newGuardPosition.x] = direction
 		guardPosition = newGuardPosition
-		uniqueVisitedPositions[newGuardPosition] = true
+		uniqueVisitedPositions[newGuardPosition] = direction
+		// part 2, check if a legal rotation would place the guard in a position already visited with the same direction it had the first time
+		potentialLoopDirection := rotateMove[direction]
+		potentialLoopPosition := moveGuard(guardPosition, potentialLoopDirection)
+		for {
+			if _, ok := uniqueVisitedPositions[potentialLoopPosition]; ok { // the potential position is a visited position
+				if uniqueVisitedPositions[potentialLoopPosition] == potentialLoopDirection { // the direction is the same as the first time
+					fmt.Printf("Found a loop! The position is %v and the direction is %s\n", potentialLoopPosition, potentialLoopDirection)
+					loops++
+					break
+				}
+			}
+			// if the potential position is not a visited position, continue in the same direction and try again until a visited position is found or the guard exits the map
+			potentialLoopPosition = moveGuard(potentialLoopPosition, potentialLoopDirection)
+			if outsideMap(potentialLoopPosition, labMap) {
+				break
+			}
+		}
 	}
-	fmt.Printf("The number of unique visited positions is %d\n", len(uniqueVisitedPositions))
+	fmt.Printf("Part 1. The number of unique visited positions is %d\n", len(uniqueVisitedPositions))
+	fmt.Printf("Part 2. The number of loops is %d\n", loops)
 }
