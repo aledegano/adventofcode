11aa2c85cd451051843bd91230b267c1dc55d5f2
Alessandro Degano <a.degano@gmail.com>
2024-12-06 11:41:17 +0100

    Complete part 1 of day 6 2024

--- 2024/day06/day06.go
+++ 2024/day06/day06.go
@@ -8,7 +8,7 @@ import (
 	// "regexp"
 	// "strconv"
 	"strings"
-	"time"
+	// "time"
 	// "unicode"
 )
 
@@ -19,21 +19,21 @@ func main() {
 	flag.Parse()
 	bytes, err := os.ReadFile(*inputFile)
 	if err != nil {
-		fmt.Printf("Error reading file %s: %v\n", *inputFile, err)
+		//fmt.Printf("Error reading file %s: %v\n", *inputFile, err)
 		return
 	}
 	contents := string(bytes)
 	lines := strings.Split(contents, "\n")
-	labMap := make([][]string, len(lines))
+	labMap := make([][]string, len(lines)-1)
 	type Position struct {
 		x, y int
 	}
 	guardPosition := Position{0, 0}
 	move := map[string]Position{
-		"<": Position{0, -1},
-		">": Position{0, 1},
-		"^": Position{-1, 0},
-		"v": Position{1, 0},
+		"<": Position{-1, 0},
+		">": Position{1, 0},
+		"^": Position{0, -1},
+		"v": Position{0, 1},
 	}
 	rotateMove := map[string]string{
 		"<": "^",
@@ -55,29 +55,37 @@ func main() {
 			}
 		}
 	}
+	//fmt.Println("Lab map: ", labMap)
+	//fmt.Printf("Guard position and direction: %d, %d, %s\n", guardPosition.x, guardPosition.y, labMap[guardPosition.y][guardPosition.x])
 	newGuardPosition := guardPosition
 	for {
 		newGuardPosition = Position{
 			guardPosition.x + move[labMap[guardPosition.y][guardPosition.x]].x,
 			guardPosition.y + move[labMap[guardPosition.y][guardPosition.x]].y,
 		}
-		fmt.Printf("New Guard position, and direction: %d, %d, %s\n", newGuardPosition.x, newGuardPosition.y, labMap[newGuardPosition.y][newGuardPosition.x])
-		time.Sleep(1 * time.Second)
-		if labMap[newGuardPosition.y][newGuardPosition.x] == "#" { // Rotate 90 degrees to the right
-			newGuardPosition = Position{
-				guardPosition.x + move[rotateMove[labMap[guardPosition.y][guardPosition.x]]].x,
-				guardPosition.y + move[rotateMove[labMap[guardPosition.y][guardPosition.x]]].y,
-			}
-			fmt.Printf("New Guard position, and direction: %d, %d, %s\n", newGuardPosition.x, newGuardPosition.y, labMap[newGuardPosition.y][newGuardPosition.x])
-			time.Sleep(1 * time.Second)
-		}
-		// if the guard exited the map break the loop
+		newMove := labMap[guardPosition.y][guardPosition.x]
+		//fmt.Println("Boundary check: ", newGuardPosition.x, newGuardPosition.y, len(labMap[0]), len(labMap))
 		if newGuardPosition.x < 0 || newGuardPosition.x >= len(labMap[0]) || newGuardPosition.y < 0 || newGuardPosition.y >= len(labMap) {
+			//fmt.Println("Guard exited the map")
 			break
 		}
+		//fmt.Printf("Tentative new guard position, and direction: %d, %d, %s\n", newGuardPosition.x, newGuardPosition.y, labMap[newGuardPosition.y][newGuardPosition.x])
+		//time.Sleep(1 * time.Second)
+		if labMap[newGuardPosition.y][newGuardPosition.x] == "#" { // Rotate 90 degrees to the right
+			newMove = rotateMove[labMap[guardPosition.y][guardPosition.x]]
+			newGuardPosition = Position{
+				guardPosition.x + move[newMove].x,
+				guardPosition.y + move[newMove].y,
+			}
+		}
+		labMap[guardPosition.y][guardPosition.x] = "."
+		labMap[newGuardPosition.y][newGuardPosition.x] = newMove
+		//fmt.Printf("Confirmed new guard position, and direction: %d, %d, %s\n", newGuardPosition.x, newGuardPosition.y, labMap[newGuardPosition.y][newGuardPosition.x])
+		//time.Sleep(1 * time.Second)
+		// if the guard exited the map break the loop
 		guardPosition = newGuardPosition
-		fmt.Printf("Guard position and direction: %d, %d, %s\n", guardPosition.x, guardPosition.y, labMap[guardPosition.y][guardPosition.x])
-		time.Sleep(1 * time.Second)
+		//fmt.Printf("Set guard position and direction: %d, %d, %s\n", guardPosition.x, guardPosition.y, labMap[guardPosition.y][guardPosition.x])
+		//time.Sleep(1 * time.Second)
 		uniqueVisitedPositions[newGuardPosition] = true
 	}
 	fmt.Printf("The number of unique visited positions is %d\n", len(uniqueVisitedPositions))
