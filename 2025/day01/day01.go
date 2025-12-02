8aa3195f6736b31dde45d6f55dd7685e91511d97
Alessandro Degano <a.degano@gmail.com>
2025-12-02 10:24:03 +0100

    start work on part2 not solved

--- 2025/day01/day01.go
+++ 2025/day01/day01.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	// "math"
+	"math"
 	"os"
 	// "regexp"
 	"strconv"
@@ -24,21 +24,41 @@ func main() {
 	contents := string(bytes)
 	lines := strings.Split(contents, "\n")
 	position := 50
+	newPosition := 0
 	password := 0
+	across := 0
 	for _, line := range lines {
 		if len(line) < 1 {
 			break
 		}
 		diff, _ := strconv.Atoi(line[1:])
-		if string(line[0]) == "L" {
-			position = position - diff
-		} else {
-			position = position + diff
+		if *debug {
+			fmt.Println("Current position:", position, "Instruction:", line, "Diff:", diff)
 		}
+		if string(line[0]) == "L" {
+			newPosition = position - diff
+		} else {
+			newPosition = position + diff
+		}
+		if (newPosition < 0 && position > 0) || (newPosition > 0 && position < 0) {
+			across = across + 1
+			if *debug {
+				fmt.Println("New position:", newPosition, "Old position:", position)
+				fmt.Println("Crossed zero, across is now", across)
+			}
+		}
+		if int(math.Abs(float64(newPosition))) > 100 {
+			across = across + int(math.Floor(math.Abs(float64(newPosition))/100))
+			if *debug {
+				fmt.Println("Multiple turns, across is now", across)
+			}
+		}
+		position = newPosition
 		position = position % 100
 		if position == 0 {
 			password = password + 1
 		}
 	}
-	fmt.Println(password)
+	fmt.Println("Part 1 solution:", password)
+	fmt.Println("Part 2 solution:", password+across)
 }
